0411f4001acc90607b09b5443529b1a81c8768ee
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-12-03 17:44:27 +0500

    fix brand logo styling

--- src/app/sections/brand-logo/BrandLogo.js
+++ src/app/sections/brand-logo/BrandLogo.js
@@ -1,33 +1,32 @@
 export default function BrandLogos() {
-    const brands = [
-      { name: 'Axon', logo: '/assest/brand-logo/image 31.png' },
-      { name: 'Jetstar', logo: '/assest/brand-logo/image 30.png' },
-      { name: 'Expedia', logo: '/assest/brand-logo/image 29.png' },
-      { name: 'Qantas', logo: '/assest/brand-logo/image 28.png' },
-      { name: 'Alitalia', logo: '/assest/brand-logo/image 27.png' },
-    ];
-  
-    return (
-      <div className="py-16 bg-white">
-        <div className="max-w-6xl mx-auto px-4">
-          <div className="flex justify-center items-center space-x-8">
-            {brands.map((brand, index) => (
-              <div
-                key={index}
-                className={`p-4 transition-transform hover:scale-110 ${
-                  brand.name === 'Expedia' ? 'bg-white shadow-lg rounded-lg' : ''
-                }`}
-              >
-                <img
-                  src={brand.logo}
-                  alt={brand.name}
-                  className="h-12 object-contain"
-                />
-              </div>
-            ))}
-          </div>
+  const brands = [
+    { name: 'Axon', logo: '/assest/brand-logo/image 31.png' },
+    { name: 'Jetstar', logo: '/assest/brand-logo/image 30.png' },
+    { name: 'Expedia', logo: '/assest/brand-logo/image 29.png' },
+    { name: 'Qantas', logo: '/assest/brand-logo/image 28.png' },
+    { name: 'Alitalia', logo: '/assest/brand-logo/image 27.png' },
+  ];
+
+  return (
+    <div className="py-20 bg-white">
+      <div className="max-w-screen">
+        <div className="flex justify-between items-center">
+          {brands.map((brand, index) => (
+            <div
+              key={index}
+              className="p-4 transition-transform hover:scale-110 hover:bg-white hover:shadow-lg hover:rounded-lg"
+            >
+              <img
+                src={brand.logo}
+                alt={brand.name}
+                className="h-12 2xl:h-16 object-contain"
+              />
+            </div>
+          ))}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
+}
+
   
