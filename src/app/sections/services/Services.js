1f9078833165c3989cf64e81cfc0fce705c2a207
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-12-04 17:55:40 +0500

    fix hover and slider issue

--- src/app/sections/services/Services.js
+++ src/app/sections/services/Services.js
@@ -38,7 +38,9 @@ const ServicesSection = () => {
         <section className="py-20">
             {/* Section Header */}
             <div className="text-center mb-12">
-                <p className="text-[22px] lg:text-[19px] 2xl:text-[32px] text-[#5E6282] font-poppins font-semibold mb-3">CATEGORY</p>
+                <p className="text-[22px] lg:text-[19px] 2xl:text-[32px] text-[#5E6282] font-poppins font-semibold mb-3">
+                    CATEGORY
+                </p>
                 <h2 className="text-[38px] sm:text-[45px] lg:text-[32px] 2xl:text-[65px] desktop:text-[74px] md:text-[48px] lg:text-[53px] text-[#14183E] font-volkhov font-bold pb-14">
                     We Offer Best Services
                 </h2>
@@ -46,41 +48,58 @@ const ServicesSection = () => {
 
             {/* Service Cards */}
             <div className="flex flex-wrap justify-center gap-16 xl:justify-between 2xl:justify-center desktop:justify-between">
+
                 {services.map((service, index) => (
                     <div
                         key={index}
-                        className="w-[280px] md:w-[220px] lg:w-[200px] 2xl:w-[340px] desktop:w-[320px] flex flex-col items-center relative"
+                        className="w-[280px] md:w-[220px] lg:w-[200px] 2xl:w-[340px] desktop:w-[320px] flex flex-col 
+                        items-center relative group "
                     >
-                        {/* Background image with dynamic position */}
+                        <section className="z-10 hover:bg-white rounded-2xl transition-all duration-300 hover:scale-105 hover:shadow-lg hover:p-2">
+                            {/* Background image with dynamic position */}
+                            <div
+                                className="absolute transition-all duration-300 opacity-70 group-hover:opacity-100"
+                                style={{
+                                    top: `${service.bgPosition.top}rem`,
+                                    left: `${service.bgPosition.left}rem`
+                                }}
+                            >
+                                <img
+                                    alt="Background Shape"
+                                    src={service.bgImage}
+                                    className="w-full h-full object-contain"
+                                />
+                            </div>
+                            {/* Foreground image */}
+                            <div className="w-22 h-20 relative z-10 transition-all duration-300 group-hover:scale-110">
+                                <img
+                                    src={service.icon}
+                                    alt={`${service.title} icon`}
+                                    className="w-full h-full object-contain"
+                                />
+                            </div>
+                            <div className="text-center pt-10">
+                                <h3 className="text-[18px] lg:text-[18px] 2xl:text-[36px] text-[#1E1D4C] font-semibold mb-2 tracking-wide 2xl:tracking-normal transition-all duration-300 group-hover:text-[#14183E]">
+                                    {service.title}
+                                </h3>
+                                <p className="text-[18px] lg:text-[16px] 2xl:text-[27px] text-[#5E6282] mb-2 tracking-wide transition-all duration-300 group-hover:text-[#1E1D4C]">
+                                    {service.description}
+                                </p>
+                            </div>
+                        </section>
+
+
+                        {/* Hover Image */}
                         <div
-                            className="absolute"
-                            style={{
-                                top: `${service.bgPosition.top}rem`,
-                                left: `${service.bgPosition.left}rem`
-                            }}
+                            className="absolute top-32 right-20 w-full h-full flex justify-center items-center opacity-0 transition-opacity duration-300 group-hover:opacity-100 z-0"
                         >
                             <img
-                                alt="Background Shape"
-                                src={service.bgImage}
-                                className="w-full h-full object-contain"
+                                src="/assest/services/Rectangle.png"
+                                alt="Hover Background"
+                                className="w-auto h-auto object-contain"
                             />
                         </div>
-                        {/* Foreground image */}
-                        <div className="w-22 h-20 relative z-10">
-                            <img
-                                src={service.icon}
-                                alt={`${service.title} icon`}
-                                className="w-full h-full object-contain"
-                            />
-                        </div>
-                        <div className="text-center pt-10">
-                            <h3 className="text-[18px] lg:text-[20px] 2xl:text-[36px] text-[#1E1D4C] font-semibold mb-2 tracking-wide 2xl:tracking-normal">
-                                {service.title}
-                            </h3>
-                            <p className="text-[18px] lg:text-[18px] 2xl:text-[27px] text-[#5E6282] mb-2 tracking-wide">
-                                {service.description}
-                            </p>
-                        </div>
+
                     </div>
                 ))}
             </div>
@@ -89,4 +108,3 @@ const ServicesSection = () => {
 };
 
 export default ServicesSection;
-
