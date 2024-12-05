c0bf1f6b3e80d60ffd8d58d646369155ecddbfb0
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-12-05 16:51:19 +0500

    fix subscribe section

--- src/app/sections/subscribe-section/SubscribeSection.js
+++ src/app/sections/subscribe-section/SubscribeSection.js
@@ -2,13 +2,13 @@ export default function SubscribeSection() {
   return (
     <section>
       <div className="bg-purple-50 rounded-tl-large rounded-b-2xl mt-24 py-24 relative">
-        <div className="px-8 lg:px-32 text-center">
-          <h1 className="text-[20px] sm:text-[24px] lg:text-[32px] 2xl:text-[40px] dektop:text-[54px] leading-8 md:leading-large text-[#5E6282] font-bold mb-16 2xl:mb-32">
+        <div className="px-8 lg:px-32 flex flex-col items-center text-center">
+          <h1 className="text-[20px] sm:text-[24px] lg:text-[32px] 2xl:text-[33px] dektop:text-[54px] 2xl:w-[859px] leading-8 md:leading-large text-[#5E6282] font-bold mb-16 2xl:mb-32">
             Subscribe to get information, latest news and other interesting offers about Cobham
           </h1>
 
           <div className="flex flex-col gap-10 lg:flex-row justify-center items-center space-x-4">
-            <div className="flex items-center bg-white rounded-lg p-2 w-full max-w-[420px] 2xl:max-w-[700px] z-10">
+            <div className="flex items-center bg-white rounded-lg p-2 w-full max-w-[420px] z-10">
             <img
           src="/assest/subscribe/Group 46.png"
           className="w-auto h-auto"
@@ -18,10 +18,10 @@ export default function SubscribeSection() {
                 type="email"
                 placeholder="Your email"
                 className="w-full 2xl:w-[400px] px-4
-              py-2 text-gray-600 focus:outline-none 2xl:text-[38px]"
+              py-2 text-gray-600 focus:outline-none 2xl:text-[14px]"
               />
             </div>
-            <button className="bg-orange-500 2xl:text-[38px] text-white px-8 py-4 rounded-lg hover:bg-orange-600">
+            <button className="bg-orange-500 2xl:text-[17px] text-white px-8 py-4 rounded-lg hover:bg-orange-600">
               Subscribe
             </button>
           </div>
