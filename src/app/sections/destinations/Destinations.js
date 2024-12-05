a65124d3ddcf99b5b682470d5d94bde57ecac1be
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2024-12-05 14:39:12 +0500

    fix services and destionation section

--- src/app/sections/destinations/Destinations.js
+++ src/app/sections/destinations/Destinations.js
@@ -21,11 +21,11 @@ const destinations = [
 
 const Destinations = () => {
     return (
-        <section className="py-16 bg-white">
+        <section className="py-28 bg-white mr-6 sm:mr-0">
             {/* Section Header */}
             <div className="container mx-auto text-center mb-12">
-                <p className="text-[22px] sm:text-[28px] lg:text-[19px] 2xl:text-[32px] text-[#5E6282] font-poppins font-semibold mb-3">Top Selling</p>
-                <h2 className="text-[38px] sm:text-[48px] lg:text-[40px] 2xl:text-[74px] md:text-[48px] lg:text-[53px] text-[#14183E] font-volkhov font-bold pb-2">
+                <p className="text-[22px] sm:text-[28px] lg:text-[19px] 2xl:text-[18px] text-[#5E6282] font-poppins font-semibold mb-3">Top Selling</p>
+                <h2 className="text-[38px] sm:text-[48px] lg:text-[40px] 2xl:text-[50px] md:text-[48px] lg:text-[53px] text-[#14183E] font-volkhov font-bold pb-2">
                     Top Destinations
                 </h2>
             </div>
@@ -35,17 +35,17 @@ const Destinations = () => {
                 {destinations.map((destination, index) => (
                     <div
                         key={index}
-                        className="w-[280px] sm:w-[400px] lg:w-[320px] xl:w-[290px] 2xl:w-[360px] desktop:w-[420px] bg-white shadow-md rounded-2xl overflow-hidden transition-transform transform hover:scale-105"
+                        className="w-[280px] sm:w-[400px] lg:w-[320px] xl:w-[290px] 2xl:w-[314px] desktop:w-[420px] bg-white shadow-md rounded-2xl overflow-hidden transition-transform transform hover:scale-105"
                     >
                         {/* Image */}
                         <img
                             src={destination.image}
                             alt={destination.title}
-                            className="w-full h-auto object-cover"
+                            className="w-full h-[310px] desktop:h-[412px] object-cover"
                         />
 
                         {/* Card Content */}
-                        <div className="p-6 text-[20px] sm:text-[25px] lg:text-[17px] 2xl:text-[28px] text-[#5E6282]">
+                        <div className="p-6 text-[20px] sm:text-[25px] lg:text-[17px] 2xl:text-[18px] text-[#5E6282]">
                             <div className="flex justify-between">
                                 <h3>
                                     {destination.title}
--- src/app/sections/services/Services.js
+++ src/app/sections/services/Services.js
@@ -35,30 +35,24 @@ const services = [
 
 const ServicesSection = () => {
     return (
-        <section className="py-20 relative max-h-screen xl:max-h-[610px] 2xl:max-h-screen">
+        <section className="py-10 mr-4 sm:mr-0 relative xl:max-h-[610px] 2xl:h-screen">
 
             {/* Section Header */}
             <div className="text-center mb-12">
-                <p className="text-[22px] lg:text-[19px] 2xl:text-[32px] text-[#5E6282] font-poppins font-semibold mb-3">
+                <p className="text-[22px] lg:text-[19px] 2xl:text-[18px] text-[#5E6282] font-poppins font-semibold mb-3">
                     CATEGORY
                 </p>
-                <h2 className="text-[38px] sm:text-[45px] lg:text-[32px] 2xl:text-[65px] desktop:text-[74px] md:text-[48px] lg:text-[53px] text-[#14183E] font-volkhov font-bold pb-14">
+                <h2 className="text-[38px] sm:text-[45px] lg:text-[32px] 2xl:text-[50px] desktop:text-[50px] md:text-[48px] lg:text-[53px] text-[#14183E] font-volkhov font-bold pb-14">
                     We Offer Best Services
                 </h2>
             </div>
-
-            {/* <img
-                                src="/assest/services/Rectangle.png"
-                                alt="Hover Background"
-                                className="w-auto h-auto 2xl:w-[220px] object-contain"
-                            /> */}
 
             {/* Service Cards */}
             <div className="flex flex-wrap justify-center gap-16 xl:justify-between 2xl:justify-center desktop:justify-between relative">
                 {services.map((service, index) => (
                     <div
                         key={index}
-                        className="w-[280px] md:w-[220px] lg:w-[200px] 2xl:w-[340px] desktop:w-[320px] flex flex-col 
+                        className="w-[280px] md:w-[220px] lg:w-[200px] 2xl:w-[190px] 2xl:hover:w-[210px] desktop:w-[250px] desktop:hover:w-[260px] flex flex-col 
                         items-center relative group"
                     >
                         <section className="z-10 hover:bg-white rounded-2xl transition-all duration-300 hover:scale-105 hover:shadow-lg hover:p-2 relative">
@@ -85,10 +79,10 @@ const ServicesSection = () => {
                                 />
                             </div>
                             <div className="text-center pt-10">
-                                <h3 className="text-[18px] lg:text-[18px] 2xl:text-[36px] text-[#1E1D4C] font-semibold mb-2 tracking-wide 2xl:tracking-normal transition-all duration-300 group-hover:text-[#14183E]">
+                                <h3 className="text-[18px] lg:text-[18px] 2xl:text-[20px] text-[#1E1D4C] font-semibold mb-2 tracking-wide 2xl:tracking-normal transition-all duration-300 group-hover:text-[#14183E]">
                                     {service.title}
                                 </h3>
-                                <p className="text-[18px] lg:text-[16px] 2xl:text-[27px] text-[#5E6282] mb-2 tracking-wide transition-all duration-300 group-hover:text-[#1E1D4C]">
+                                <p className="text-[18px] lg:text-[16px] 2xl:text-[16px] text-[#5E6282] mb-2 tracking-wide transition-all duration-300 group-hover:text-[#1E1D4C]">
                                     {service.description}
                                 </p>
                             </div>
@@ -96,12 +90,12 @@ const ServicesSection = () => {
 
                         {/* Hover Image */}
                         <div
-                            className="absolute top-32 right-32 lg:top-36 lg:right-20 2xl:right-36 w-full h-full flex justify-center items-center opacity-0 transition-opacity duration-300 group-hover:opacity-100 z-0 pointer-events-none"
+                            className="absolute top-32 right-32 lg:top-36 lg:right-20 2xl:top-28 2xl:right-30 desktop:right-32 w-full h-full flex justify-center items-center opacity-0 transition-opacity duration-300 group-hover:opacity-100 z-0 pointer-events-none"
                         >
                             <img
                                 src="/assest/services/Rectangle.png"
                                 alt="Hover Background"
-                                className="w-auto h-auto 2xl:w-[220px] object-contain"
+                                className="w-auto h-auto 2xl:w-[135px] object-contain"
                             />
                         </div>
                     </div>
